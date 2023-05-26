c4c5ea7aeeecc419358f2fdddf39d9c907e7c7f5
Dmast00 <obed.hinojosa@outlook.com>
2023-05-26 12:49:06 -0600

    se agrego json

--- locustfile.py
+++ locustfile.py
@@ -2,11 +2,19 @@ import Excelfile as ef
 from locust import HttpUser, task,between
 import jsonBuilder as builder
 
+file_path = 'assets/test.xlsx'
+assigned_variables = ef.assign_excel_columns_to_variables(file_path)
+endpoint_url = "/"
 class Myuser(HttpUser):
     wait_time = between(5,10)
     @task
     def my_task(self):
-        self.client.get("")
+        json = {
+            "idBrand" : assigned_variables['IdBrand'],
+            "idDealer" : assigned_variables['IdDealer'],
+            "idSubBrand" : assigned_variables['IdSubbrand']
+        }
+        self.client.get("{endpoint_url}",data=json)
 
 def main():
     file_path = 'assets/test.xlsx'
