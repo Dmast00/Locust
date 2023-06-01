3d078419d8541b0bfa10acba59fbee76c64ffeb5
Obed Hinojosa Enriquez <ohinojosa@telepro.com.mx>
2023-05-31 19:06:54 -0500

    modificaciones

---
+++ functions.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+import urllib
+
+def get_hidden_elements(response):
+        # look for aspNetHidden elements
+        soup = BeautifulSoup(response.text, 'html.parser')
+        view_st = urllib.parse.quote(soup.find(id="__VIEWSTATE")['value'], safe='')
+        view_st_gen = urllib.parse.quote(soup.find(id="__VIEWSTATEGENERATOR")['value'], safe='')
+        # event_val = urllib.parse.quote(soup.find(id="__EVENTVALIDATION")['value'], safe='')
+        # returns the payload elements
+        return view_st, view_st_gen #, event_val
+
+
+# Return headers requests
+def get_headers(www, route):
+    headers = {
+        # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        # 'Accept-Encoding': 'gzip, deflate, br',
+        # 'Accept-Language': 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3',
+        # 'Connection': 'keep-alive',
+        # 'Content-Length': '194183',
+        # 'Content-Type': 'application/x-www-form-urlencoded',
+        # # 'DNT': '1',
+        # 'Host': www,
+        # 'Origin': www,
+        # 'Referer': www + route,
+        # 'Upgrade-Insecure-Requests': '1'
+        # # 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:84.0) Gecko/20100101 Firefox/84.0'
+        "Host": www,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": "514",
+        "Origin": www,
+        "Connection": "keep-alive",
+        "Referer": www+route,
+        "Cookie": "ASP.NET_SessionId=ukorehpfkcfowzposl54qqqw",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-User": "?1"
+    }
+    return headers
--- locustfile.py
+++ locustfile.py
@@ -1,33 +1,63 @@
 import Excelfile as ef
 from locust import HttpUser, task,between
 import jsonBuilder as builder
+import functions as func
 
 file_path = 'assets/test.xlsx'
 assigned_variables = ef.assign_excel_columns_to_variables(file_path)
+# Server ip or url to which the tests will be directed
+www = 'http://localhost:29456'
+# Timeout for every request
+timeout_set = 60
+
+
 class Myuser(HttpUser):
-    @task
-    def my_task(self):
-        endpoint_url = "/Classification.svc/V2/List"
-        json = {
-            "idBrand" : assigned_variables['IdBrand'],
-            "idDealer" : assigned_variables['IdDealer'],
-            "idSubBrand" : assigned_variables['IdSubBrand']
-        }
-        self.client.get(endpoint_url,data=json)
+    def on_start(self):
+        self.login()
+    wait_time = between(2,10)
 
-    @task
-    def ProductInfo(self):
-        endpoint_url = "/ProductInfo.svc/V2/List/"
-        json = {
-            "idBrand" : assigned_variables['IdBrand'],
-            "idSubBrand" : assigned_variables['IdSubBrand'],
-            "idClassification" : assigned_variables['IdClassification'],
-            "idYear" : assigned_variables['Year'],
-            "idVersion" : assigned_variables['IdVersion'],
-            "idDealer" : assigned_variables['IdDealer'],
-        }
-        self.client.get(endpoint_url,data=json)
+    # @task
+    # def PanelPrincipal(self):
+    #     url ="/ProdeskNet.html"
+    #     self.client.get(url)
 
+    #iniciar sesion
+    def login(self):
+        username = "admintelepro"
+        password = "Generico.101"
+
+        # request and set state variables for next request
+        self.view_st, self.view_st_gen = func.get_hidden_elements(
+            self.client.request("GET", "", name="1 Ini Get: /"))
+        
+        # initial login
+        url_main = "/login.aspx"
+        # payload raw text
+        p_main = "__LASTFOCUS=&__EVENTTARGET=&__EVENTARGUMENT=&"
+        p_main = p_main + "__VIEWSTATE=" + self.view_st + "&__VIEWSTATEGENERATOR=" + self.view_st_gen
+        p_main = p_main + "%2FwEdAAjtrfc9bFlycPRNvd9GpAk4QUnBeTmJnrNhxmPOJ04KLLXwdJFqioFGQdCtVz1hJa0z0pbtemMeEG4g1PnXXStT1O%2F2WkqbDud7RMyNsu3snL7gUW5RdHnCkit5S48%2F25KHg6q%2FhgwvZ%2BXsyHR25sWJxIKTJ3pG1f%2FYoIrbO%2F2ZOR9sxDmPtN2zrGIyTBh9GJ%2BqACo%2F64K6tJ5UstwGY7Y0&txtUsu="+username+"&txtPwd="+password+"&btnAceptar=Ingresar&usuAcceso=&intentosAcceso=0"
+        response = self.client.post(url_main, data=p_main, timeout=timeout_set)
+        # response = self.client.post(url_main,headers=func.get_headers(www,url_main),data=p_main)
+        if response.status_code == 200:
+            print("Todo ok")
+        else:
+            print("Algo salio mal")
+
+    
+    
 
 if __name__ == "__main__":
     Myuser()
+
+
+
+
+    # @task
+    # def my_task(self):
+    #     endpoint_url = "/Classification.svc/V2/List"
+    #     json = {
+    #         "idBrand" : assigned_variables['IdBrand'],
+    #         "idDealer" : assigned_variables['IdDealer'],
+    #         "idSubBrand" : assigned_variables['IdSubBrand']
+    #     }
+    #     self.client.get(endpoint_url,data=json)
